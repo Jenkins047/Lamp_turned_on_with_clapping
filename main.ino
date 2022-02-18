02123de818734a946471e51aa92a6dcb6b481c51
Szymon Kędzierski <69625790+Jenkins047@users.noreply.github.com>
2022-02-18 21:26:51 +0100

    Beta claping device

--- main.ino
+++ main.ino
@@ -1,38 +1,28 @@
-#define OPAMP 3
-#define GATE 2
+#define OPAMP A0
+#define GATE 3
 
 void setup()
 {
     pinMode(GATE, OUTPUT);
     digitalWrite(GATE, LOW);
-    pinMode(OPAMP, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(OPAMP), time, CHANGE);
 }
 
-unsigned long start = 0, stop = 0xFFFFFFFFFFFFFFFF;
 uint8_t gate_state = LOW;
+byte event = 0;
 
 void loop()
 {
-    if(stop - start < 25)
+    if(map(analogRead(OPAMP), 0, 1023, 0, 5) > 3)
     {
+        event++;
+        delay(26);
+    }
+
+    if(event >= 2)
+    {
+        event = 0;
         gate_state = (gate_state + 1) % 2;
         digitalWrite(GATE, gate_state);
-        stop = 0xFFFFFFFFFFFFFFFF;
-        start = 0;
-
-        attachInterrupt(digitalPinToInterrupt(OPAMP), time, CHANGE);
     }
-}
 
-void time()
-{
-    detachInterrupt(digitalPinToInterrupt(OPAMP));
-    
-    uint8_t state = digitalRead(OPAMP);
-    
-    if(state == HIGH)
-        start = millis(); //start counter
-    else
-        stop = millis(); //stop counter
 }
