80db1f95c5f97d558d5d050fb7fd65c0a56319f4
Szymon Kędzierski <69625790+Jenkins047@users.noreply.github.com>
2022-02-19 15:55:05 +0100

    Reprogram implementation of clapping detection

--- main.ino
+++ main.ino
@@ -5,24 +5,51 @@ void setup()
 {
     pinMode(GATE, OUTPUT);
     digitalWrite(GATE, LOW);
+    Serial.begin(9600);
 }
 
 uint8_t gate_state = LOW;
-byte event = 0;
+long timeStamp = 0;
+double voltage = 0;
 
 void loop()
 {
-    if(map(analogRead(OPAMP), 0, 1023, 0, 5) > 3)
+    if(voltage = map(analogRead(OPAMP), 0, 1023, 0, 5) > 4)
     {
-        event++;
-        delay(26);
-    }
+        timeStamp = millis();
+        delay(5);
+        Serial.println("Clap_1!");
 
-    if(event >= 2)
-    {
-        event = 0;
-        gate_state = (gate_state + 1) % 2;
-        digitalWrite(GATE, gate_state);
-    }
+        while(1)
+        {
+            if(voltage = map(analogRead(OPAMP), 0, 1023, 0, 5) > 4)
+            {
+                if(millis() - timeStamp > 10 && millis() - timeStamp < 1000)
+                {
+                    gate_state = (gate_state + 1) % 2;
+                    digitalWrite(GATE, gate_state);
 
+                    Serial.println("Clap_2!");
+                    if(gate_state)
+                        Serial.println("Led ON!");
+                    else
+                        Serial.println("Led OFF!");
+
+                    delay(30);
+                    break;
+                }
+                else
+                {
+                    break;
+                }   
+            }
+
+            if(millis() - timeStamp > 1000)
+            {
+                Serial.println("Break!");
+                break;
+            }
+            
+        }
+    }
 }
